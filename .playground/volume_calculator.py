79157168431251a738fc7dcb9519da5115f2bcfe
root <root@localhost>
2021-10-29 23:46:04 +0600

    auto publish 1635529555100

---
+++ .playground/volume_calculator.py
@@ -0,0 +1,11 @@
+def volume(x,y,z):
+    calculate_volume=x*y*z
+    print(calculate_volume)
+    print("c.c")
+length=input("length:")
+length_n=float(length)
+width=input("width:")
+width_n=float(width)
+hight=input("hight:")
+hight_n=float(hight)
+volume(length_n,width_n,hight_n)
---
+++ codes/cmn2.py
@@ -0,0 +1,10 @@
+def volume(x,y,z):
+    mult_cmn=x*y*z
+    print(mult_cmn)
+length=input("length:")
+length_n=float(length)
+width=input("width:")
+width_n=float(width)
+hight=input("hight:")
+hight_n=float(hight)
+cmn(length_n,width_n,hight_n)
